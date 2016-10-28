b22c64f2fe70637657faf2819e810178f4bb816d
Armins Stepanjans <arminbagrat@localhost.localdomain>
2016-10-28 19:48:41 +0530

    Rename cThruster to control and extract it

---
+++ Thruster library/control.py
@@ -0,0 +1,69 @@
+# Class for calculating and sending signals to thrusters
+
+class control:
+    """ Stores thruster force values and handles sending them.
+    """
+
+    force = [none] * 7
+    direction = 'none'
+
+    def updateForce(joystickAngle, rsy_val, ltrig_val, rtrig_val):
+        """ Updates locally stored thruster force values and direction.
+
+        Arguments:
+        joystickAngle -- angle that the coordinate of steering joystick make with the x-axis
+        rsy_val -- y value of right joystick
+        ltrig_val -- value of left trigger (must be modified for Windows, because on win both triggers are read as single variable)
+        rtrig_val -- value of right trigger (also must be modified for Windows)
+        """
+        if ang >= 45 and ang < 135:
+            direction = 'F'
+            force[1] = rsy_val
+            force[2] = rsy_val
+            force[3] = 0
+            force[4] = 0
+        elif ang >= 135 and ang < 225:
+            direction = 'L'
+            force[1] = rsy_val
+            force[2] = 0
+            force[3] = 0
+            force[4] = rsy_val
+        elif ang >= 225 and ang < 315:
+            direction = 'B'
+            force[1] = 0
+            force[2] = 0
+            force[3] = rsy_val
+            force[4] = rsy_val
+        elif ang >= 315 or ang < 45:
+            direction = 'R'
+            force[1] = 0
+            force[2] = rsy_val
+            force[3] = rsy_val
+            force[4] = 0
+        if ltrig_val != 0:
+            force[5] = -ltrig_val
+            force[6] = -ltrig_val
+        elif rtrig_val != 0:
+            force[5] = rtrig_val
+            force[6] = rtrig_val
+
+    def send(self):
+        """ Convert thruster force values from percent to PWM values and send them.
+        """
+        for i in range(1, 7):
+            force = map(force[i], -100, 100, 1140, 1855)
+            force = str(force)
+            ser.write(force)
+        ser.write('E')
+
+    def sendNull(self):
+        """ Stops all thrusters
+        """
+        ser.write('T')
+        ser.write('T')
+        for i in range(1, 7):
+            force = map(0, -100, 100, 1140, 1855)
+            force = str(force)
+            ser.write(force)
+        ser.write('E')
+
--- Thruster library/main.py
+++ Thruster library/main.py
@@ -2,6 +2,7 @@
 # Created by WSROV team
 import pygame
 import serial
+from control import *
 from helper import *
 from pygame.locals import *
 from time import sleep
@@ -10,11 +11,9 @@ port = ""     # Used in init() don't change
 ser = ""      # Will be defined as a serial port
 timeout = 10  # Timeout for communication with Master in seconds
 
-cThruster = cThruster()
+control = control()
 
 pDirection = 'none'
-
-random -= 5
 
 # Xbox controller button IDs
 a_but = 0
@@ -161,97 +160,18 @@ def main():
         # Detects and stores direction of left joystick
         # Stores force values of each thruster (in percent of their max F)
         ang = angle(lsx_val, lsy_val)
-        pDirection = cThruster.direction
+        pDirection = control.direction
 
-        cThruster.updateForce(ang, rsy_val, ltrig_val, rtrig_val)
+        control.updateForce(ang, rsy_val, ltrig_val, rtrig_val)
 
         # If horizontal direction has changed
         # sends zeros to all corner thrusters first
+        control.sendNull()
 
         # sends force values of each thruster to Master
-        cThruster.send()
+        control.send()
 
 
-class cThruster:
-    """ Stores thruster force values and handles sending them.
-    """
-
-    force = [none] * 7
-    direction = 'none'
-
-    def updateForce(joystickAngle, rsy_val, ltrig_val, rtrig_val):
-        """ Updates locally stored thruster force values and direction.
-
-        Arguments:
-        joystickAngle -- angle that the coordinate of steering joystick make with the x-axis
-        rsy_val -- y value of right joystick
-        ltrig_val -- value of left trigger (must be modified for Windows, because on win both triggers are read as single variable)
-        rtrig_val -- value of right trigger (also must be modified for Windows)
-        """
-        if ang >= 45 and ang < 135:
-            direction = 'F'
-            force[1] = rsy_val
-            force[2] = rsy_val
-            force[3] = 0
-            force[4] = 0
-        elif ang >= 135 and ang < 225:
-            direction = 'L'
-            force[1] = rsy_val
-            force[2] = 0
-            force[3] = 0
-            force[4] = rsy_val
-        elif ang >= 225 and ang < 315:
-            direction = 'B'
-            force[1] = 0
-            force[2] = 0
-            force[3] = rsy_val
-            force[4] = rsy_val
-        elif ang >= 315 or ang < 45:
-            direction = 'R'
-            force[1] = 0
-            force[2] = rsy_val
-            force[3] = rsy_val
-            force[4] = 0
-        if ltrig_val != 0:
-            force[5] = -ltrig_val
-            force[6] = -ltrig_val
-        elif rtrig_val != 0:
-            force[5] = rtrig_val
-            force[6] = rtrig_val
-
-    def send(self):
-        """ Convert thruster force values from percent to PWM values and send them.
-        """
-        for i in range(1, 7):
-            force = map(force[i], -100, 100, 1140, 1855)
-            force = str(force)
-            ser.write(force)
-        ser.write('E')
-
-    def sendNull(self):
-        """ Stops all thrusters
-        """
-        ser.write('T')
-        ser.write('T')
-        for i in range(1, 7):
-            force = map(0, -100, 100, 1140, 1855)
-            force = str(force)
-            ser.write(force)
-        ser.write('E')
-
-
-# Class that defines properties for each individual thruster
-class thruster:
-    # Function automatically executed upon creation of a thruster object
-    def __init__(self,
-                 num,         # ID of a thruster
-                 lb=1140,   # Lower bound of PWM that will be sent to a thruster
-                 ub=1855):  # Upper bound of PWM that will be sent to a thruster
-        self.num = num
-        self.lb = lb
-        self.ub = ub
-
-# Function to send values to an ESC through Arduinos
 
 init()
 main()
