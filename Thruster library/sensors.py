3e59c35ed4a516a278dc23d0a164ad90ae5fcd45
Armins Stepanjans <arminbagrat@localhost.localdomain>
2016-11-09 19:39:08 +0530

    Create get_hum()

--- Thruster library/sensors.py
+++ Thruster library/sensors.py
@@ -16,3 +16,22 @@ def get_temp(ser):
 
    temp = ser.read(2)
    return temp
+
+def get_hum(ser):
+    """ Requests and returns temperature
+
+    Arguments:
+    ser -- serial port object to which Master is connected
+
+    Returns:
+    Relative humidity readings from sensor on Slave in percent
+    """
+
+    ser.write('S')
+    ser.write('S')
+    ser.write('h')
+    ser.write('h')
+    ser.write('E')
+
+    hum = ser.read(2)
+    return temp
