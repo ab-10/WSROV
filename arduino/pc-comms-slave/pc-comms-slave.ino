af169ea83e3a46b719a0a414f90857fef53898c7
Markhamsan Lyndem <Markhamsan Lyndem>
2016-11-16 15:40:20 +0530

    Add Requests to Slave

--- arduino/pc-comms-slave/pc-comms-slave.ino
+++ arduino/pc-comms-slave/pc-comms-slave.ino
@@ -4,7 +4,7 @@
 #include <Wire.h>
 #include <Servo.h>
 
-#define DHTPIN 2
+#define DHTPIN 3
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
 
@@ -12,18 +12,20 @@ Servo T1, T2, T3, T4, T5, T6;
 Servo Thrusters[] = {T1, T2, T3, T4, T5, T6};
 const int tPins[] = {3, 5, 6, 9, 10, 11}; // digital pins used to communicate with ESCs
 int tForce[6]; // stores force values for each thruster
-byte read[2]; // stores raw readings from Master
+byte read; // stores raw readings from Master
 
 float hum = 0;
 float temp = 0;
 byte first = 0;
 byte second = 0;
+int i = 0;
 
 
 void setup() {
     Serial.begin(9600);
     Wire.begin(8);
     Wire.onReceive(receiveEvent);
+    Wire.onRequest(requestEvent);
     for (int n = 0; n++; n < 6){
         Thrusters[n].attach(tPins[n]);
     }
@@ -37,28 +39,30 @@ void loop() {
     }
 }
 
-void receiveEvent(){
-  int i = 0;
-  while( Wire.available() > 0){
-    read[i] = Wire.read();
-    i++;
-  }
-  
-  switch (read[0]){
-    case 'h':
+void receiveEvent(int howMany){
+  while(Wire.available()){
+    read = Wire.read();
+    
+    if (read == 1){
       hum = dht.readHumidity();
       first = hum;
       Wire.write(first);
       second = hum - first;
       second *= 100;
       Wire.write (second);
+    }
 
-    case 't':
+    if (read == 2){
       temp = dht.readTemperature();
       first = temp;
       Wire.write(first);
       second = temp - first;
       second *= 100;
       Wire.write (second);
+    }
   }
 }
+void requestEvent(int howMuch){
+     Wire.write(3);
+     Wire.write(3);
+  }
