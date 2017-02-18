395ba1c48591670f74925c4629a6a6ca677e4f57
Armins Stepanjans <arminbagrat@localhost.localdomain>
2017-02-18 19:56:45 +0530

    Create a seperate com class

---
+++ main/communication.py
@@ -0,0 +1,10 @@
+""" Handles communication with the robot
+"""
+def send(port, type, byte1='!', byte2='!', byte3='!', byte4='!'):
+ 
+    port.write(type)
+    port.write(byte1)
+    port.write(byte2)
+    port.write(byte3)
+    port.write(byte4)
+    port.write('E')
