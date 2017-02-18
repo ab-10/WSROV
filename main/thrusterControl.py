b99d1ca3ef86781af940ff17dcba63be78c96696
Armins Stepanjans <arminbagrat@localhost.localdomain>
2017-02-18 20:03:59 +0530

    Change values stored in force[] to pulse lengths

--- main/thrusterControl.py
+++ main/thrusterControl.py
@@ -1,5 +1,6 @@
 # Class for calculating and sending signals to thrusters
 import helper
+import communication
 
 class thruster:
     """ Stores thruster force values and handles sending them.
@@ -17,22 +18,21 @@ class thruster:
         ltrig_val -- value of left trigger (must be modified for Windows, because on win both triggers are read as single variable)
         rtrig_val -- value of right trigger (also must be modified for Windows)
         """
-        self.force[0] = rsy_val
-        self.force[1] = lsx_val
+        self.force[0] = helper.map(rsy_val, -100, 100, 1140, 1850)
+        self.force[1] = helper.map(lsx_val, -100, 100, 1140, 1850)
 
     def send(self, ser):
         """ Convert thruster force values from percent to PWM values and send them.
         """
+        ser.write('T')
         for i in range(0, 2):
-            force = helper.map(self.force[i], -100, 100, 1140, 1855)
-            force = str(force)
-            ser.write(force)
+            ser.write(force[i] // 100)
+            ser.write(force[i] % 100)
         ser.write('E')
 
     def sendNull(self, ser):
         """ Stops all thrusters
         """
-        ser.write('T')
         ser.write('T')
         for i in range(0, 2):
             force = helper.map(0, -100, 100, 1140, 1855)
