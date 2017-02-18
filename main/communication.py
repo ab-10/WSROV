86eeb88a319dddaf36a42ea960aeb16b05970c16
Armins Stepanjans <arminbagrat@localhost.localdomain>
2017-02-18 21:43:54 +0530

    Remove empty line

--- main/communication.py
+++ main/communication.py
@@ -1,7 +1,6 @@
 """ Handles communication with the robot
 """
 def send(port, type, byte1='!', byte2='!', byte3='!', byte4='!'):
- 
     port.write(type)
     port.write(byte1)
     port.write(byte2)
