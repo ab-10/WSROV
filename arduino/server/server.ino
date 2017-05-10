61550b9207fe873dc91f631b3a18d00b9183ea44
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-05-10 11:42:45 +0530

    Remove archaic code

--- arduino/server/server.ino
+++ arduino/server/server.ino
@@ -20,9 +20,6 @@ void setup()  {
   Udp.begin(localPort);
 
   Serial.begin(9600);
-    while (!Serial) {
-    ; // wait for serial port to connect. Needed for Leonardo only
-  }
 }
 
 void loop() {
