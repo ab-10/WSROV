dc296a4da809a5c9d79662f18314e2cf43497b22
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-05-11 13:59:25 +0530

    Return humidity

--- main/sensors.py
+++ main/sensors.py
@@ -2,14 +2,14 @@ import communication
 
 def get_temp(port):
     """ Requests and returns temperature
-    
+
     Arguments:
     ser -- serial port object to which Master is connected
-    
+
     Returns:
     Temperature readings from sensor on Slave in Celsius
     """
-    
+
     communication.send(port, 'S', 't')
 
     temp = port.read(2)
@@ -24,8 +24,8 @@ def get_hum(port):
     Returns:
     Relative humidity readings from sensor on Slave in percent
     """
-    
+
     communication.send(port, 'S', 'h')
 
     hum = port.read(2)
-    return temp
+    return hum
