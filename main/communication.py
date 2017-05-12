af03862029d80f65c154405e10d4f2befffa8ea4
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-05-12 17:11:06 +0530

    Disable timeout

--- main/communication.py
+++ main/communication.py
@@ -1,17 +1,14 @@
-import time
-
 """ sends data to Master using simple com protocol
 """
 def send(port, type, byte1='!', byte2='!', byte3='!', byte4='!'):
-    msg = type + str(byte1) + str(byte2) + str(byte3) + str(byte4) + 'E'
+    msg = type + byte1 + byte2 + byte3 + byte4 + 'E'
     port.write(msg.encode('ascii'))
     port.flush()
 
 """ reads from Master
 """
 def read(port):
-    startTime = time.time()
     reading = b""
-    while (reading == b"") and (time.time() - startTime < 2):
+    while reading == b"":
         reading = port.readline().replace(b"\r\n", b"")
     return reading
